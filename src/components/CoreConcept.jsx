50e4270933778d576ed840177c6a69bb64a143bf
Luka <lgurjidze@gmail.com>
2024-12-28 20:30:39 +0400

    configure props and split up components

---
+++ src/components/CoreConcept.jsx
@@ -0,0 +1,16 @@
+export default function CoreConcept(props) {
+    return (
+      <li>
+        <img src={props.image}></img>
+  
+        <h3>
+          {props.title}
+        </h3>
+  
+        <p>
+          {props.description}
+        </p>
+  
+      </li>
+    )
+  }

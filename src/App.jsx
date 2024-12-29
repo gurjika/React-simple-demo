250ef3c3d06fa33465656323505b1c2fb02a1303
Luka <lgurjidze@gmail.com>
2024-12-29 13:17:06 +0400

    add selected topic to usestate and configure conditionals

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { CORE_CONCEPTS } from './data.js';
+import { useState } from 'react'
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState();
+
 
   function handleSelect(selectedButton) {
-
-  }
+    setSelectedTopic(selectedButton);
+  } 
 
 
 
@@ -40,6 +43,18 @@ function App() {
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
 
           </menu>
+            {selectedTopic === undefined ? <p>Please select a topic.</p> : null}
+            {selectedTopic !==undefined ?           
+            <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div> 
+          : null}
 
 
         </section>
